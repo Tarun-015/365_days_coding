82fc84e706b5eb45194ee9e4228aa9bc442635cb
Tarun Chaudhary <tarunn1510@gmail.com>
2025-06-26 19:15:57 +0530

    Add files via upload

---
+++ Day-66(java-practice)/Pattern4.java
@@ -0,0 +1,13 @@
+//Right-Aligned Triangle
+public class Pattern4 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+            for (int s = 1; s <= n - i; s++)
+                System.out.print("  ");
+            for (int j = 1; j <= i; j++)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
