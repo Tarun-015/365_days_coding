7a956c509d1108244b749db956bad211784ff70c
Tarun Chaudhary <tarunn1510@gmail.com>
2025-07-02 19:35:26 +0530

    Add files via upload

---
+++ Day-72(java-practice)/Pattern20.java
@@ -0,0 +1,17 @@
+//Cross (X) Star Pattern
+
+public class Pattern20 {
+    public static void main(String[] args) {
+        int n = 5; // should be odd
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j == i || j == (n - i + 1))
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
