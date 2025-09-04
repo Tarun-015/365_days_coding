911195adb0b44959919abfa2853f98709a789fa0
Tarun Chaudhary <tarunn1510@gmail.com>
2025-09-04 16:08:03 +0530

    Delete Day-123(python-practice)/Day-124(python-practice) directory

--- Day-123(python-practice)/Day-124(python-practice)/Point.py
+++
@@ -1,9 +0,0 @@
-# Problem 1: Slots Restriction
-
-class Point:
-    __slots__ = ('x', 'y')
-    def __init__(self, x, y):
-        self.x, self.y = x, y
-
-p = Point(1, 2)
-# p.z = 10  # Raises: AttributeError
--- Day-123(python-practice)/Day-124(python-practice)/README.md
+++
@@ -1,3 +0,0 @@
-Slots
-
-dataclass
