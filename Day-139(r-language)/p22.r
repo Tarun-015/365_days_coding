1f9061d41af9b6375c37fe44ac5ced6d3e0c9839
Tarun Chaudhary <tarunn1510@gmail.com>
2025-09-29 22:51:51 +0530

    Add files via upload

---
+++ Day-139(r-language)/p22.r
@@ -0,0 +1,10 @@
+# Q22. Factor levels and frequency counts
+genders <- factor(c("M","F","M","M","F","F","M","F"))
+print("Factor Levels:")
+print(levels(genders))
+
+print("Frequency Table:")
+print(table(genders))
+
+
+# Concepts: Factors, categorical data handling
