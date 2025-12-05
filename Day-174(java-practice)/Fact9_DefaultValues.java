1c7810d2393e363e1e6643316f3e020b3b69f1a4
Tarun Chaudhary <tarunn1510@gmail.com>
2025-12-05 19:13:29 +0530

    Add files via upload

---
+++ Day-174(java-practice)/Fact9_DefaultValues.java
@@ -0,0 +1,12 @@
+// Default Values for Class Variables
+public class Fact9_DefaultValues {
+    static int num;
+    static boolean flag;
+    static String text;
+
+    public static void main(String[] args) {
+        System.out.println(num);   // 0
+        System.out.println(flag);  // false
+        System.out.println(text);  // null
+    }
+}
